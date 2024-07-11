2181faaf32704d585cb961a4ca7971937c1a4c15
demisu-s <damisusefu@gmail.com>
2024-07-11 13:23:05 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>My First Vite Project</h1>
+    </div>
+  );
+}
+
+export default App;
